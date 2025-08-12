d8cd9c009411fe17512f479f696508467dcab269
Anishproject <123952532+Anishisgood@users.noreply.github.com>
2025-08-12 16:50:00 +0530

    Removed some

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import  from "../angular-frontend/src/app/components/theme-toggle/theme-toggle.component"
-
-export default function SyntheticV0PageForDeployment() {
-  return < />
-}
